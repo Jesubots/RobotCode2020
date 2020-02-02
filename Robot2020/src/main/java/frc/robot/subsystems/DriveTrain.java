94db4116116446e0b6a880a7a360a817cde95331
bkeefe313 <bkeefe313@gmail.com>
2020-02-01 18:53:33 -0600

    working on dt

--- Robot2020/src/main/java/frc/robot/subsystems/DriveTrain.java
+++ Robot2020/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -18,22 +18,28 @@ import frc.robot.RobotContainer;
 public class DriveTrain extends SubsystemBase {
 
 
-  private WPI_TalonSRX left_front_talon = new WPI_TalonSRX(0);
-  private WPI_TalonSRX left_back_talon = new WPI_TalonSRX(1);
-  private WPI_TalonSRX right_front_talon = new WPI_TalonSRX(2);
-  private WPI_TalonSRX right_back_talon = new WPI_TalonSRX(3);
+  private WPI_TalonSRX left_front = new WPI_TalonSRX(0);
+  private WPI_TalonSRX left_follower = new WPI_TalonSRX(1);
+  private WPI_TalonSRX right_front = new WPI_TalonSRX(2);
+  private WPI_TalonSRX right_follower = new WPI_TalonSRX(3);`
 
-  private SpeedControllerGroup dt_left = new SpeedControllerGroup(left_back_talon, left_front_talon);
-  private SpeedControllerGroup dt_right = new SpeedControllerGroup(right_back_talon, right_front_talon);
+  private SpeedControllerGroup dt_left = new SpeedControllerGroup(left_follower, left_front);
+  private SpeedControllerGroup dt_right = new SpeedControllerGroup(right_follower, right_front);
 
   private DifferentialDrive driveTrain = new DifferentialDrive(dt_left, dt_right);
 
   public DriveTrain() {
-
+    left_follower.setControlMode(ControlMode.follower, 0);
+    right_follower.setControlMode(ControlMode.follower, 2);
+    left_front.
   }
 
   @Override
   public void periodic() {
     driveTrain.tankDrive(Robot.driver_stick.getY(), Robot.driver_stick_2.getY());
+  }
+
+  public double getDistance() {
+
   }
 }
